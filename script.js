c32aec0bcee6a82a4961c69728e61624e1fb1675
helipatel05 <helibenpatel9999@gmail.com>
2026-04-23 22:16:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// Import the functions you need from the SDKs you needimport { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyCoDeCyvWUapIpOuu9fPtmhG7IXCN6tA08",
   authDomain: "smart-parking-system-6a312.firebaseapp.com",
@@ -16,7 +13,7 @@ const firebaseConfig = {
   measurementId: "G-WK8XCDKTN4"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
